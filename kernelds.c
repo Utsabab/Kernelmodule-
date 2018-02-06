a88e6ce48aa6dbfa68a008ddcc637e015d83ff13
Utsabab <utsab.khakurel@bison.howard.edu>
2018-02-06 13:08:43 -0500

    traverse hashtable

--- kernelds.c
+++ kernelds.c
@@ -22,8 +22,7 @@ static void initialise_ht(void) {
 	}
 }
 
-static void addto_ht(struct birthday add_bday)
-    {
+static void addto_ht(struct birthday add_bday) {
         unsigned long hash = 5381;
         int c;
 	char* str = &add_bday.name[0];
@@ -32,15 +31,25 @@ static void addto_ht(struct birthday add_bday)
             hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	printk (KERN_INFO "%d %s", hash % 5, add_bday.name);
         list_add(&(add_bday.list), &(hasht[hash % 5].list));
-    }
+}
 
-int create_ht_init(void) {
-
-	struct birthday person1, person2, person3, person4, person5;
-	struct birthday *tmp;
+static void traverse_ht(void) {
 	struct list_head *pos, *q;
+	struct birthday *tmp;
 
-	struct birthday birthday_list;
+	int i = 0;
+	printk(KERN_INFO "traversing the list using list_for_each()\n");
+	for (i; i<5; i++) {
+		//list_for_each(pos, &hasht[i].list) {
+			//tmp = list_entry(pos, struct birthday, list);
+			//printk(KERN_INFO "bucket= %d name= %s day= %d month= %d year=%d", i, tmp->name, tmp->day, tmp->month, tmp->year);
+		//}
+	}
+}
+
+static int create_ht_init(void) {
+	struct birthday person1, person2, person3, person4, person5;
+
 	initialise_ht();
 	
 	strncpy(person1.name,"Ut",100);
@@ -70,45 +79,15 @@ int create_ht_init(void) {
 	
 	printk(KERN_INFO "%s", person1.name);
 	
-	
 	addto_ht(person1);
 	addto_ht(person2);
 	addto_ht(person3);
 	addto_ht(person4);
 	addto_ht(person5);
+	
+	traverse_ht();
 	return 0;
 }
-
-/**
-list_add(&(person1.list), &(birthday_list.list));
-list_add(&(person2.list), &(birthday_list.list));
-list_add(&(person3.list), &(birthday_list.list));
-	
-printk("\n");
-
-printk("traversing the list using list_for_each()\n");
-list_for_each(pos, &birthday_list.list){
-		tmp = list_entry(pos, struct birthday, list);
-		printk("day= %d month= %d year=%d", tmp->day, tmp->month, tmp->year);
-		
-	}
-	printk("\n");
-
-	printk("traversing the list using list_for_each()\n");
-	list_for_each_entry(tmp, &birthday_list.list, list)
-		printk("day= %d month= %d year=%d", tmp->day, tmp->month, tmp->year);
-	printk("\n");
-
-	printk("deleting the list using list_for_each_safe()\n");
-	list_for_each_safe(pos, q, &birthday_list.list){
-		tmp= list_entry(pos, struct birthday, list);
-		printk("freeing item day= %d month= %d year=%d\n", tmp->day, tmp->month, tmp->year);
-		list_del(pos);
-		kfree(tmp);
-	}
-	return 0;
-}	
-**/
 
 void simple_exit(void){
 	printk(KERN_INFO "Removing Module\n");
